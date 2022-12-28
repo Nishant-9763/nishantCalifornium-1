a58c2783c580d773070a4eb61c5ba68c86a14bb7
Nishnat-9763 <nishnatgupta9763@gmail.com>
2022-12-28 21:05:54 +0530

    28 dec assignment

--- src/controllers/user1Controller.js
+++ src/controllers/user1Controller.js
@@ -3,61 +3,80 @@ const jwt= require("jsonwebtoken")
 const { get } = require("../routes/route")
 
 const createUser= async function(req,res){
-    let data= req.body
+   try {let data= req.body
 
     let savedData= await MyUserModel.create(data)
 
-    res.send({status: true, Msg: savedData})
+    res.status(201).send({status: true, Msg: savedData})
+
+  }catch(Error){
+    console.log({status: false, Msg:Error.message});
+}
 }
 
 const loginUser= async function(req,res){
-    let {emailId, password}= req.body
+   try{ let {emailId, password}= req.body
 
     let savedData= await MyUserModel.findOne({emailId,password})
     if(!savedData){
-       return res.send({status: false, Error: "user not present"})
+       return res.status(401).send({status: false, Error: "user not present"})
     }
 
     let token= jwt.sign({userId: savedData._id.toString()},'FunctionVeryUp')
     if(!token){
-        res.send({status:false, Error: "enter token"})
+        res.status(401).send({status:false, Error: "enter token"})
     }
 
-    res.send({status: true, Msg: token})
+    res.status(201).send({status: true, Msg: token})
+
+    }catch(Error){
+    console.log({status: false, Msg: Error.message});
+}
 }
 
 const getUser= async function(req,res){
-    let data= req.params.userId
+   try{ let data= req.params.userId
 
     let checkId= await MyUserModel.findById(data)
 
-    res.send({status: true, Msg: checkId})
+    res.status(200).send({status: true, Msg: checkId})
+
+    }catch(Error){
+        console.log({status: false, msg:Error.message});
+    }
 }
 
 const updateUser= async function(req,res){
-    let userId= req.params.userId
+  try{  let userId= req.params.userId
 
     let checkUserId = await MyUserModel.findById(userId)
     if(!checkUserId){
-        return res.send({status: false, Error: "userId is not valid"})
+        return res.status(401).send({status: false, Error: "userId is not valid"})
     }
 
     let data= req.body.firstName
 
     let savedData= await MyUserModel.findOneAndUpdate({_id:userId},{firstName: data},{new: true})
-    res.send({status: true, Msg: savedData})
+    res.status(201).send({status: true, Msg: savedData})
+
+    }catch(Error){
+        console.log({status: false, msg: Error.message});
+    }
 }
 
 
 const deleteUser= async function(req,res){
-    let userId= req.params.userId
+ try{   let userId= req.params.userId
     let checkUserId = await MyUserModel.findById(userId)
     if(!checkUserId){
-        return res.send({status: false, Error: "userId is not valid"})
+        return res.status(401).send({status: false, Error: "userId is not valid"})
     }
 
     let savedData= await MyUserModel.findOneAndUpdate({_id:userId},{isDeleted: true},{new: true})
-    res.send({status:true, Msg: savedData})
+    res.status(201).send({status:true, Msg: savedData})
+}catch(Error){
+    console.log({status: false, msg: Error.message});
+}
 }
 
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,30 +3,36 @@ const jwt= require('jsonwebtoken')
 const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
-    let token= req.headers['x-auth-token']
+   try {let token= req.headers['x-auth-token']
     // console.log(token);
     if(!token){
-        return res.send({status: false, Msg: "token is must"})
+        return res.status(401).send({status: false, Msg: "token is must"})
     }
     let decodedToken= jwt.verify(token,'FunctionVeryUp')
     if(!decodedToken){
-        return res.send({status: false, Error: "token is not valid"})
+        return res.status(401).send({status: false, Error: "token is not valid"})
     }
     req.decodedToken=decodedToken
 
     next()
+}catch(Error){
+    console.log({status: false, msg:Error.message});
+}
 }
 
 
 const authorise = function(req, res, next) {
     // comapre the logged in user's id and the id in request
     // console.log(req.decodedToken);
-    let token1= req.decodedToken
+   try{ let token1= req.decodedToken
     // console.log(token1);
     if (token1.userId !=req.params.userId){
-        return res.send({status: false,Msg: "userId not match"  })
+        return res.status(401).send({status: false,Msg: "userId not match"  })
     }
     next()
+}catch(Error){
+    console.log({status: false,msg: Error.message});
+}
 }
 
 module.exports.authenticate=authenticate
